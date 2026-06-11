23a7c8e2528eec60072cf7d039bbc904ab54b522
Mateus Luiz <mateuslfreitasb@gmail.com>
2026-06-11 09:30:24 -0300

    Add multiple tilesets to maps

---
+++ game/assets/tiles/bedroom_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="bedroom_tileset" tilewidth="16" tileheight="16" tilecount="169" columns="13">
+ <image source="bedroom_tileset.png" width="208" height="208"/>
+</tileset>
