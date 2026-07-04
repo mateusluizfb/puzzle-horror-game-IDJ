9dd6967cf70c0b10c29576e8ab51e0be1d857e04
Mateus Luiz <mateuslfreitasb@gmail.com>
2026-07-04 12:54:12 -0300

    Supress logs in non-debug builds

--- engine/include/utils/Log.h
+++ engine/include/utils/Log.h
@@ -8,19 +8,35 @@ class Log
 {
   public:
     static void info(const std::string &message) {
+    #ifndef DEBUG
+        (void)message;
+    #else
         std::cout << "\033[34mINFO\033[0m: " << message << std::endl; // blue
+    #endif
     }
 
     static void warning(const std::string &message) {
+    #ifndef DEBUG
+        (void)message;
+    #else
         std::cout << "\033[33mWARNING\033[0m: " << message << std::endl; // yellow
+    #endif
     }
 
     static void error(const std::string &message) {
+    #ifndef DEBUG
+        (void)message;
+    #else
         std::cerr << "\033[31mERROR\033[0m: " << message << std::endl; // red
+    #endif
     }
 
     static void debug(const std::string &message) {
+    #ifndef DEBUG
+        (void)message;
+    #else
         std::cout << "\033[32mDEBUG\033[0m: " << message << std::endl; // green
+    #endif
     }
 };
 
